b1686359657cc44e5b96abde198b5d64413901d4
Your Name <you@example.com>
2023-09-20 03:02:38 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Expense Tracker</h1>
+      {/* <p>Make Your Budget</p> */}
+    </div>
+  );
+};
+export default Header;
